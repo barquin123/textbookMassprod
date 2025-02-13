adac6256176e92bdbf82fd90ab8afdccaa42e67c
JayFrank\FDC.JayFrank-NC-FE <fdc.jayfrankbarquin@gmail.com>
2025-02-13 15:59:50 +0800

    icons

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ app.on('ready', () => {
         nodeIntegration: true,
         preload: path.join(__dirname, 'preload.js')
       },
+      icon: path.join(__dirname, 'assets/icons/mug.ico')
     });
 
     app.on('activate', () => {
